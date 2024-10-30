4a7bf752b9915061281c34f12dd950d30a7015de
Jikku Jose <jikku@storybrain.com>
2024-10-30 10:33:24 +0530

    Remove unwanted

--- src/utils/ShapeShift.js
+++
@@ -1,9 +0,0 @@
-export default class ShapeShift {
-  url(from, to) {
-    return `https://shapeshift.io/rate/${from}_${to}`
-  }
-
-  extract(json, that) {
-    that.setState(state => ({ rate: json["rate"], error: json["error"] }))
-  }
-}
